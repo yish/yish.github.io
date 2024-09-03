017ca4f38d0c77b252a40c3dc96566fb81c2b1ea
yish <yishaym@gmail.com>
2024-09-03 14:29:46 +0300

    Update script.js

--- faces/script.js
+++ faces/script.js
@@ -64,12 +64,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function createFireworks(num) {
-        for (let i = 0; i < num; i++) {
-            fireworks.push(new Firework());
-        }
-    }
-
     function Particle(x, y, color) {
         this.x = x;
         this.y = y;
@@ -96,7 +90,61 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function Balloon() {
-        // ... (keep the existing Balloon code)
+        this.x = Math.random() * canvas.width;
+        this.y = canvas.height + 50;
+        this.speed = Math.random() * 1 + 0.5;
+        this.size = 50;
+        this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        this.popped = false;
+
+        this.update = function() {
+            if (!this.popped) {
+                this.y -= this.speed;
+                if (this.y <= 0) {
+                    this.pop();
+                }
+            }
+        }
+
+        this.draw = function() {
+            if (!this.popped) {
+                ctx.save();
+                ctx.translate(this.x, this.y);
+
+                // Draw balloon body
+                ctx.beginPath();
+                ctx.moveTo(0, 0);
+                ctx.bezierCurveTo(this.size/2, -this.size/2, this.size/2, -this.size, 0, -this.size);
+                ctx.bezierCurveTo(-this.size/2, -this.size, -this.size/2, -this.size/2, 0, 0);
+                ctx.fillStyle = this.color;
+                ctx.fill();
+
+                // Draw balloon knot
+                ctx.beginPath();
+                ctx.moveTo(-3, 0);
+                ctx.lineTo(3, 0);
+                ctx.lineTo(0, 5);
+                ctx.closePath();
+                ctx.fillStyle = this.color;
+                ctx.fill();
+
+                // Draw "picture" circle
+                ctx.beginPath();
+                ctx.arc(0, -this.size/2, this.size/4, 0, Math.PI * 2);
+                ctx.fillStyle = 'white';
+                ctx.fill();
+                ctx.strokeStyle = 'rgba(0,0,0,0.3)';
+                ctx.stroke();
+
+                ctx.restore();
+            }
+        }
+
+        this.pop = function() {
+            this.popped = true;
+            createSparkles(this.x, this.y);
+            playSound(523.25, 0.1); // Play a short high-pitched sound
+        }
     }
 
     function createSparkles(x, y) {
@@ -109,8 +157,41 @@ document.addEventListener('DOMContentLoaded', function() {
         return Math.random() * (max - min) + min;
     }
 
+    function createFireworks(num) {
+        for (let i = 0; i < num; i++) {
+            fireworks.push(new Firework());
+        }
+    }
+
     function animate() {
-        // ... (keep the existing animate code)
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        fireworks.forEach((fw, index) => {
+            fw.update();
+            fw.draw();
+
+            if (fw.sy >= random(-3, 0) || fw.size <= 0.2) {
+                for (let i = 0; i < 100; i++) {
+                    particles.push(new Particle(fw.x, fw.y, fw.color));
+                }
+                fireworks.splice(index, 1);
+            }
+        });
+
+        particles.forEach((p, index) => {
+            p.update();
+            p.draw();
+            if (p.life <= 0) particles.splice(index, 1);
+        });
+
+        balloons.forEach((balloon, index) => {
+            balloon.update();
+            balloon.draw();
+            if (balloon.popped && balloon.y < 0) balloons.splice(index, 1);
+        });
+
+        requestAnimationFrame(animate);
     }
 
     animate();
